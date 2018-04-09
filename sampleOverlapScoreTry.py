3a6db57c252ce2605217747e6a5d23c3f89adff4
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-09 21:44:49 +0200

    Code change

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -85,7 +85,7 @@ alignment = {}
 pairCount = 1
 for seq1, seq2 in itertools.combinations(sequences, 2):
     al = pairwise2.align.globalxx(sequences[seq1].seq,sequences[seq2].seq)
-    alignment[pairCount] = sequences[seq1].id + ";" + al[0][0] + ";" + str(sequences[seq1].letter_annotations["phred_quality"]) + ";" + sequences[seq2].id + ";" + al[0][1] +  ";" + str(sequences[seq2].letter_annotations["phred_quality"])
+    alignment[pairCount] = sequences[seq1].id + ";" + al[0][0] + ";" + str(sequences[seq1].letter_annotations["phred_quality"]).strip('[]') + ";" + sequences[seq2].id + ";" + al[0][1] +  ";" + str(sequences[seq2].letter_annotations["phred_quality"]).strip('[]')
     #print(alignment)
     #print(alignment[pairCount].split(",")[1][3])
     #print("\n ******************************** \n")
@@ -96,10 +96,10 @@ for seq1, seq2 in itertools.combinations(sequences, 2):
 for key in alignment:
     #print(alignment[key])
     #print(alignment[key].split(",")[0])
-    print(alignment[key].split(";")[5])
-    #overlapScore = overalpScoreCalculation(alignment[key],20)
-    #alignment[key] = alignment[key] + ";" + str(overlapScore)
-    #print(alignment[key].split(";")[6])
+    #print(alignment[key].split(";")[5])
+    overlapScore = overalpScoreCalculation(alignment[key],20)
+    alignment[key] = alignment[key] + ";" + str(overlapScore)
+    print(alignment[key].split(";")[6])
     print("\n ############ \n")
  
 
