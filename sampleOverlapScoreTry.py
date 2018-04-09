a4609a1a91099a13345f292e41b4b9cf80ef2a5c
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-09 20:58:45 +0200

    Code change

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -33,9 +33,9 @@ def probabilityQ (X, b, p):
 """
 def getProbQuality (q):
     print(q)
-    #q = int(q)
-    #p = 10**(-q/10)
-    return q
+    q = int(q)
+    p = 10**(-q/10)
+    return p
 
 
 nt = ["A","T","C","G"] # Bases
@@ -58,14 +58,14 @@ def overalpScoreCalculation(seqDetails, i):
         
             # Gap in first read -> calculation based on read 2
         if seqDetails.split(",")[1][i1] == "-":
-           probabilityBase = (3/13 * getProbQuality(seqDetails.split(",")[5][i2])) + (10/13 * (1 - getProbQuality(seqDetails.split(",")[5][i2])))
+           probabilityBase = (3/13 * getProbQuality(float(seqDetails.split(",")[5][i2]))) + (10/13 * (1 - getProbQuality(float(seqDetails.split(",")[5][i2]))))
            # Gap in second read -> calculation based on read 1
         elif seqDetails.split(",")[4][i2] == "-":
-           probabilityBase = (3/13 * getProbQuality(seqDetails.split(",")[2][i1])) + (10/13 * (1 - getProbQuality(seqDetails.split(",")[2][i1])))
+           probabilityBase = (3/13 * getProbQuality(float(seqDetails.split(",")[2][i1]))) + (10/13 * (1 - getProbQuality(float(seqDetails.split(",")[2][i1]))))
            # Existing score calculation
         else:
             for n in nt:
-                probabilityBase = probabilityBase + (probabilityQ(n,seqDetails.split(",")[1][i1],getProbQuality(seqDetails.split(",")[2][i1])) * probabilityQ(n,seqDetails.split(",")[4][i2],getProbQuality(seqDetails.split(",")[5][i2])))
+                probabilityBase = probabilityBase + (probabilityQ(n,seqDetails.split(",")[1][i1],getProbQuality(float(seqDetails.split(",")[2][i1]))) * probabilityQ(n,seqDetails.split(",")[4][i2],getProbQuality(float(seqDetails.split(",")[5][i2]))))
         probabilityOverall = probabilityOverall * probabilityBase
         i1 = i1 + 1
         i2 = i2 + 1
