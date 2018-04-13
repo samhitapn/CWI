6ae2af428a6fbc1d82bab780368bc85665fb141f
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-13 19:09:06 +0200

    New Steps

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -89,7 +89,7 @@ def overalpScoreCalculation(seqDetails, i):
 # MAIN
 # Get the overlap pairs and details from the PAF files
 overlaps = open("data/Sample_AllReads_Overlaps.paf","r")
-print(overlaps)
+print(overlaps.read())
 
 
 """
