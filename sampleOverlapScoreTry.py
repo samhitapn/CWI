810c486ad394f06537011cbf9c5aa0d4e8a23b12
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-14 14:28:31 +0200

    New Steps

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -93,7 +93,7 @@ def overalpScoreCalculation(seqDetails, i):
 #print(type(overlapFile))
 print("^^^^^OVERLAPS READ^^^^^^^\n")
 # Read in the fastq files -> Usually the overalp pairs; here only the test 5 reads
-sequences = sio.to_dict(sio.parse("data/Sample_AllReads.fastq","fastq"))
+#sequences = sio.to_dict(sio.parse("data/Sample_AllReads.fastq","fastq"))
 print("^^^^^^SEQUENCES READ^^^^^^\n")
 
 overlapCount = 1
@@ -104,12 +104,12 @@ with open("data/Sample_AllReads_Overlaps.paf","r") as f:
         #print(ovlPair)
         #print("^^^^^^^^^^^^^^^^^^^^")
         print(overlapCount)
-        ovl = ovlPair.split("\t")
-        al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
-        alignment[ovl[0] + '&' + ovl[5]] = al[0][0] + ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","") + ";" + al[0][1] +  ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","")
+        #ovl = ovlPair.split("\t")
+        #al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
+        #alignment[ovl[0] + '&' + ovl[5]] = al[0][0] + ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","") + ";" + al[0][1] +  ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","")
         #print("^^^^^Alignment data^^^^^^^^^")
         overlapCount = overlapCount + 1
-print(alignment)
+#print(alignment)
 
 
 """
