227c54a9f4926073d5c3a26055a55960ea234f4d
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-14 14:29:28 +0200

    New Steps

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -104,8 +104,8 @@ with open("data/Sample_AllReads_Overlaps.paf","r") as f:
         #print(ovlPair)
         #print("^^^^^^^^^^^^^^^^^^^^")
         print(overlapCount)
-        #ovl = ovlPair.split("\t")
-        #al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
+        ovl = ovlPair.split("\t")
+        al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
         #alignment[ovl[0] + '&' + ovl[5]] = al[0][0] + ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","") + ";" + al[0][1] +  ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","")
         #print("^^^^^Alignment data^^^^^^^^^")
         overlapCount = overlapCount + 1
