0ce61bcf69340539a0ecab9158be3338a27e5875
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-14 15:25:09 +0200

    New Steps

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -101,18 +101,21 @@ alignment = {}
 # Get the overlap pairs and details from the PAF files
 with open("data/Sample_AllReads_Overlaps.paf","r") as f:
     for ovlPair in f.readlines():
+        if overlapCount <= 30:
         #print(ovlPair)
         #print("^^^^^^^^^^^^^^^^^^^^")
-        print(overlapCount)
-        ovl = ovlPair.split("\t")
-        start = time()
-        al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
-        stop = time()
-        print(start - stop)
-        #alignment[ovl[0] + '&' + ovl[5]] = al[0][0] + ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","") + ";" + al[0][1] +  ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","")
-        #print("^^^^^Alignment data^^^^^^^^^")
-        overlapCount = overlapCount + 1
-#print(alignment)
+            print(overlapCount)
+            ovl = ovlPair.split("\t")
+            start = time()
+            al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
+            stop = time()
+            print(stop - start)
+            alignment[ovl[0] + '&' + ovl[5]] = al[0][0] + ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","") + ";" + al[0][1] +  ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","")
+            #print("^^^^^Alignment data^^^^^^^^^")
+            overlapCount = overlapCount + 1
+        else:
+            break
+print(alignment)
 
 
 """
