05932d4bd5155c430c9c4ede0de0e06ecd3e528c
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-14 12:59:45 +0200

    New Steps

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -96,12 +96,13 @@ print("^^^^^OVERLAPS READ^^^^^^^\n")
 # Read in the fastq files -> Usually the overalp pairs; here only the test 5 reads
 sequences = sio.to_dict(sio.parse("data/Sample_AllReads.fastq","fastq"))
 print("^^^^^^SEQUENCES READ^^^^^^\n")
-""""
+
 overlapCount = 1
 alignment = {}
 for ovlPair in overlapFile.readlines():
     print(ovlPair)
     print("^^^^^^^^^^^^^^^^^^^^")
+    """"
     ovl = ovlPair.split("\t")
     al = pairwise2.align.globalxx(sequences[ovl[0]].seq,sequences[ovl[5]].seq)
     alignment[ovl[0] + '&' + ovl[5]] = al[0][0] + ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","") + ";" + al[0][1] +  ";" + str(sequences[ovl[0]].letter_annotations["phred_quality"]).strip('[]').replace(" ","")
