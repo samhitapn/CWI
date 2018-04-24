e2d16fe8f348b6dcbcde1d9915403f340fd050ca
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-04-24 10:46:06 +0200

    Loop correction

--- sampleOverlapScoreTry.py
+++ sampleOverlapScoreTry.py
@@ -83,7 +83,7 @@ def reorderScores (read, score):
 def getGapRegionScore (read, score, pos):
     print("POS:")
     print(pos)
-    end = ntPattern.search(read,read[pos:]).start()
+    end = re.search(ntPattern,read[pos:]).start()
     print("END:")
     print(end)
     start = pos
