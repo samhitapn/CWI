59e31125c5e7b3257dbfe70ec57cffc9ef080875
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-05-03 09:20:34 +0200

    overlapScore

--- overlapScore.py
+++ overlapScore.py
@@ -55,7 +55,7 @@ def getFinalAlignment(readData):
     start1 = readData[2]
     start2 = readData[6]
     #print(readData[2],readData[3],readData[6],readData[7],cigarSeq.count("M"),cigarSeq.count("I"),cigarSeq.count("D"))
-    for i in range(0,len(cigarSeq)):
+    for i in range(0,20):
         #print(cigarSeq[i])
         #print(i,start1)
         print(i,readData[0][start1])
