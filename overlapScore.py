d1db4f53e849e383bacfc6009b704081a6a496de
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-05-15 07:03:41 +0200

    overlapScore

--- overlapScore.py
+++ overlapScore.py
@@ -64,7 +64,7 @@ def getProbQuality (q):
     qNew = ord(q)
     #print(q,qNew)
     #p = 10**(-np.float128(q)/10)
-    p = 10 ** (-qNew/10)
+    p = 10 ** (-np.float128(qNew)/10)
     #print(p)
     return p
 
