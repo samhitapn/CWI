a29480adee116e634becd63e7759ba71278e775e
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-05-28 16:10:35 +0200

    GapsTest

--- overlapScore.py
+++ overlapScore.py
@@ -167,7 +167,7 @@ def getOverlapScore(key, readData):
 
             #if char == "M":
                 #print("!!!!", char, num, "*******")
-"""
+                """
                 for i in range(0, num):
                     probabilityBase = 0
                     #print(tempScore1[i],getProbQuality(tempScore1[i]),tempScore2[i],getProbQuality(tempScore2[i]))
@@ -182,7 +182,7 @@ def getOverlapScore(key, readData):
                     L = L + 1
                     #print("@@@@@ M-location", probabilityOverall,L,c,pos1,pos2)
                 #print("@@@@@ M-range", num,probabilityOverall, tempScore1,tempScore2, pos1,pos2,L,c)
-"""
+                """
                 pos1 = pos1 + num
                 pos2 = pos2 + num
                 #print("M:",num,pos1,pos2)
