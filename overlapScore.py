3794eb265be999a8a4cabee18c5438c9116bbfd6
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-05-29 10:15:35 +0200

    StatsChange

--- overlapScore.py
+++ overlapScore.py
@@ -233,7 +233,7 @@ for i in range(0,len(fastqData)):
 
 statFileName = args.output + "_stats.csv"
 statFile = open(statFileName,"w+")
-statFile.write("KEY \t READ1_START \t READ1_END \t READ1_OVL_LEN \t READ2_START \t READ2_END \t READ2_OVL_LEN \t MATCHES \t INSERTIONS \t DELETIONS \n")
+statFile.write("KEY \t READ1_LENGTH \t READ1_START \t READ1_END \t READ1_OVL_LEN \t READ2_LENGTH \t READ2_START \t READ2_END \t READ2_OVL_LEN \t MATCHES \t INSERTIONS \t DELETIONS \n")
 for overlapPair in pafData:
     tempData = list()
     ovl = overlapPair.split("\t")
@@ -252,7 +252,7 @@ for overlapPair in pafData:
     """
     cigarIndex = [ovl.index(i) for i in ovl if i.startswith("cg")]
     cig = getSeqFromCigar(ovl[cigarIndex[0]].split(":")[2].strip("\n"))
-    statFile.write(str(ovl[0] + "-" + ovl[5]) + "\t" + str(ovl[2]) + "\t" + str(ovl[3]) + "\t" + str(int(ovl[3]) - int(ovl[2])) + "\t" + str(ovl[7]) + "\t" + str(ovl[8]) + "\t" + str(int(ovl[8]) - int(ovl[7])) + "\t" + str(cig.count("M")) + "\t" + str(cig.count("I")) + "\t" + str(cig.count("D")) + "\n")
+    statFile.write(str(ovl[0] + "-" + ovl[5]) + "\t" + str(ovl[1]) + "\t" + str(ovl[2]) + "\t" + str(ovl[3]) + "\t" + str(int(ovl[3]) - int(ovl[2])) + "\t" + str(ovl[6]) + "\t + str(ovl[7]) + "\t" + str(ovl[8]) + "\t" + str(int(ovl[8]) - int(ovl[7])) + "\t" + str(cig.count("M")) + "\t" + str(cig.count("I")) + "\t" + str(cig.count("D")) + "\n")
     readPairData[ovl[0] + "-" + ovl[5]] = [fastqTemp[ovl[0]][0],fastqTemp[ovl[0]][1],int(ovl[2]), int(ovl[3]),fastqTemp[ovl[5]][0],fastqTemp[ovl[5]][1],int(ovl[7]),int(ovl[8]),ovl[cigarIndex[0]].split(":")[2].strip("\n")]
     #tempData = [list(fastq[ovl[0]].seq), fastq[ovl[0]].letter_annotations["phred_quality"], int(ovl[2]), int(ovl[3]), list(fastq[ovl[5]].seq),fastq[ovl[5]].letter_annotations["phred_quality"],int(ovl[7]), int(ovl[8]),ovl[20].split(":")[2].strip()]
     #readPairData[ovl[0] + "-" + ovl[5]] = [fastqTemp[ovl]]
