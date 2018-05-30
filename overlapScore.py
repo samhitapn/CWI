7a1b7cb3ddfcf440c3b3245be0facdc324b96f3f
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-05-30 11:18:25 +0200

    Code change

--- overlapScore.py
+++ overlapScore.py
@@ -193,7 +193,7 @@ def getOverlapScore(key, readData, scoreType):
                     pos2 = pos2 + num
                     #print("M:",num,pos1,pos2)
         print(alpha1,alpha2,alpha)
-        overlapScore = (np.exp(prob) ** (1/L))
+        overlapScore = (np.exp(prob) ** (1/L)) * alpha
         result.append(overlapScore)
         print(result)
     return(result)
