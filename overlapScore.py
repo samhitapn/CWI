631884401002043384465370da717cdc9fae375f
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-06-05 12:10:06 +0200

    overlapScore

--- overlapScore.py
+++ overlapScore.py
@@ -300,7 +300,7 @@ for i in gapScoreType:
     outputFile.write("KEY \t ALPHA1 \t ALPHA2 \t GAPS \t MATCHES \t ALL \t OVERLAP_TYPE \n")
 
     for key in readPairData:
-        if key == "origSeq_189-origSeq_190":
+        #if key == "origSeq_189-origSeq_190":
             keyElements = key.split("-")
             if keyElements[0].split("_")[0] == keyElements[1].split("_")[0]:
                 ovlType = "Good Overlap"
