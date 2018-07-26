69eeb41d26198e75e36f7fb90c3d04bd3a68b064
Samhita P N <samhitapn@Samhitas-MacBook-Air.local>
2018-07-26 09:15:10 +0200

    error

--- errorFreeReads.py
+++ errorFreeReads.py
@@ -35,14 +35,21 @@ def getExpandedCigar(cigar):
 
 
 # Arguement parser
-"""
+
 parser = argparse.ArgumentParser(description='Parser for input files and output files')
 parser.add_argument('-f','--file', help='File Name',required=True)
+parser.add_argument('-eb','--endbonus', help='end bonus choice',required=True)
+#parser.add_argument('-o','--old ', help='File Name',required=True)
 args = parser.parse_args()
-"""
-"""
+
+files = args.file
+files = files.split(",")
+eb = args.endbonus
+eb = eb.split(",")
 # Input data
-for file in ["origSeq", "seq1", "seq2", "seq3", "seq4", "seq5", "seq6", "seq7", "seq8", "seq9", "seq10"]:
+
+for file in files:
+    print(file)
     fastqTemp = dict()
 
     with open(file + ".sam") as s:
@@ -56,9 +63,11 @@ for file in ["origSeq", "seq1", "seq2", "seq3", "seq4", "seq5", "seq6", "seq7", 
 
     # Replacing fastq Dictionary
     #c = 0
-    with open("../fasta_100000_indel/" + file + ".fasta") as fasta:
+    with open(file + ".fasta") as fasta:
         seq = fasta.readlines()
+    c = 0
     for line in sam:
+        print(c)
         if not line.startswith("@"):
             temp = line.split("\t")
             readName = temp[0]
@@ -72,7 +81,10 @@ for file in ["origSeq", "seq1", "seq2", "seq3", "seq4", "seq5", "seq6", "seq7", 
                 print(trimLength)
                 print(reqLength)
                 print(startPos, endPos)
-            assert startPos < endPos
+            try:
+                assert startPos < endPos
+            except AssertionError:
+                continue
             sequence = seq[1][startPos:endPos]
             if len(sequence) == 0:
                 print(startPos, endPos)
@@ -80,7 +92,7 @@ for file in ["origSeq", "seq1", "seq2", "seq3", "seq4", "seq5", "seq6", "seq7", 
             fastqTemp[readName][0] = sequence
             #if readName == 'origSeq_192':
                 #print(fastqTemp[readName][0])
-        #c = c + 1
+            c = c + 1
     # Writing replaced error-free fastq file
     fileNew_fastq = file + "_errorFree.fastq"
     with open(fileNew_fastq,"w+") as fw:
@@ -90,7 +102,7 @@ for file in ["origSeq", "seq1", "seq2", "seq3", "seq4", "seq5", "seq6", "seq7", 
             #line = "@",key,n,fastqTemp[key][0],n,"+",n,fastqTemp[key][1],n
             #if key == 'origSeq_192':
                 #print(fastqTemp[key][0])
-            fw.writelines("@" + key + "\n" + fastqTemp[key][0] + "\n+\n" + fastqTemp[key][1] + "\n")
+            fw.writelines("@" + key + fastqTemp[key][0] + "\n+\n" + fastqTemp[key][1] + "\n")
             #fw.writelines(">" + key + "\n" + fastqTemp[key][0] + "\n")
 
     fileNew_fasta = file + "_errorFree.fasta"
@@ -101,48 +113,41 @@ for file in ["origSeq", "seq1", "seq2", "seq3", "seq4", "seq5", "seq6", "seq7", 
             #line = "@",key,n,fastqTemp[key][0],n,"+",n,fastqTemp[key][1],n
             #if key == 'origSeq_192':
                 #print(fastqTemp[key][0])
-            faw.writelines("@" + key + "\n" + fastqTemp[key][0] + "\n")
+            faw.writelines("@" + key + fastqTemp[key][0] + "\n")
 
 print("DONE REPLACING INDIVIDUAL FASTQ  AND FASTA")
 
 # Concatenating all files
 #cmdCat = "for f in *_errorFree.fastq; do (cat \"${f}\"; echo) >> all/allMerged_200_errorFree.fastq; done"
-cmdCat = "for f in *_errorFree.fasta; do (cat \"${f}\"; echo) >> all/allMerged_200_errorFree.fasta; done"
+mergeFile = ""
+for file in files:
+    mergeFile = mergeFile + file
+cmdCat = "for f in *_errorFree.fasta; do (cat \"${f}\"; echo) >>" + mergeFile + "_errorFree.fasta; done"
 os.system(cmdCat)
 print("DONE COMBINING FASTQ's")
+
+
 """
 # Generating overlaps from error-free reads
-os.chdir("all")
-os.system("pwd")
-for i in ["EB0","EB10","EB100","EB1000"]:
+#os.chdir("all")
+#os.system("pwd")
+for i in eb:
     print(i)
-    file_paf = "allMerged_" + i + "_fa.paf"
-    fileNew_paf = "allMerged_" + i + "_errorFree.paf"
-    cmdOld = "minimap2 -x ava-ont allMerged_200.fasta allMerged_200.fasta -c --end-bonus " + i[2:] + " > " + file_paf
-    cmdNew = "minimap2 -x ava-ont allMerged_200_errorFree.fasta allMerged_200_errorFree.fasta -c --end-bonus " + i[2:] + " > " + fileNew_paf
+    file_paf = mergeFile + i + "_error.paf"
+    fileNew_paf = mergeFile + i + "_errorFree.paf"
+    #cmdOld = "minimap2 -x ava-ont" +  mergeFile + ".fasta" +  mergeFile + ".fasta -c --end-bonus " + i[2:] + " > " + file_paf
+    cmdNew = "minimap2 -x ava-ont " +  mergeFile + "_errorFree.fasta " +  mergeFile + "_errorFree.fasta -c --end-bonus " + i[2:] + " > " + fileNew_paf
     #print(cmd)
         #os.system(cmdOld)
-        #os.system(cmdNew)
+    os.system(cmdNew)
     print("OVERLAP GENRATION DONE")
     #os.system("du -shx *|sort -rh")
-    with open(file_paf) as paf:
-        pafData = paf.readlines()
+    #with open(file_paf) as paf:
+    #    pafData = paf.readlines()
     with open(fileNew_paf) as pafNew:
         pafData_New = pafNew.readlines()
     print("PAF DATA RECEIVED")
-    with open(i + "_CIGAR.csv","w+") as oldCigar:
-        oldCigar.seek(0)
-        oldCigar.write("KEY \t GAPS \t MATCHES \t DELETIONS \t INSERTIONS \t MISMATCHES \n")
-        for oldPair in pafData:
-            ovl = oldPair.split("\t")
-            cigarIndex = [ovl.index(of) for of in ovl if of.startswith("cg")]
-            expCigar = getExpandedCigar(ovl[cigarIndex[0]].split(":")[2].strip("\n"))
-            gaps = expCigar[0].count("D") + expCigar[0].count("I")
-            subsIndex = [ovl.index(of) for of in ovl if of.startswith("NM")]
-            subsNumber = int(ovl[subsIndex[0]].split(":")[2]) - gaps
-            oldCigar.write(str(ovl[0] + "-" + ovl[5]) + "\t" + str(gaps) + "\t" + str(expCigar[0].count("M")) + "\t" + str(expCigar[0].count("D")) + "\t" + str(expCigar[0].count("I")) + "\t" + str(subsNumber) + "\n")
-    print("OLD DONE")
-    with open(i + "_errorFree_CIGAR.csv","w+") as newCigar:
+    with open(mergeFile + i + "_errorFree_CIGAR.csv","w+") as newCigar:
         newCigar.seek(0)
         newCigar.write("KEY \t GAPS \t MATCHES \t DELETIONS \t INSERTIONS \t MISMATCHES \n")
         for newPair in pafData_New:
@@ -152,14 +157,14 @@ for i in ["EB0","EB10","EB100","EB1000"]:
             gaps = expCigar[0].count("D") + expCigar[0].count("I")
             subsIndex = [ovl.index(nf) for nf in ovl if nf.startswith("NM")]
             subsNumber = int(ovl[subsIndex[0]].split(":")[2]) - gaps
-            newCigar.write(str(ovl[0] + "-" + ovl[5]) + "\t" + str(gaps) + "\t" + str(expCigar[0].count("M")) + "\t" + str(expCigar[0].count("D")) + "\t" + str(expCigar[0].count("I")) + "\t" + str(subsNumber) + "\n")
+            newCigar.write(str(ovl[0] + ":" + ovl[5]) + "\t" + str(gaps) + "\t" + str(expCigar[0].count("M")) + "\t" + str(expCigar[0].count("D")) + "\t" + str(expCigar[0].count("I")) + "\t" + str(subsNumber) + "\n")
     print("NEW DONE")
     print("PAF STATS SAVED")
 
+#####################
 
 
 
-"""
 # Parsing CIGAR string from both PAF files for gaps
 for i in ["EB0","EB10","EB100","EB1000"]:
     print(i)
@@ -193,5 +198,19 @@ for i in ["EB0","EB10","EB100","EB1000"]:
             newCigar.write(str(ovl[0] + "-" + ovl[5]) + "\t" + str(gaps) + "\t" + str(expCigar[0].count("M")) + "\t" + str(expCigar[0].count("D")) + "\t" + str(expCigar[0].count("I")) + "\n")
     print("NEW DONE")
     print("PAF STATS SAVED")
-"""
+
+
+        with open(i + "_CIGAR.csv","w+") as oldCigar:
+            oldCigar.seek(0)
+            oldCigar.write("KEY \t GAPS \t MATCHES \t DELETIONS \t INSERTIONS \t MISMATCHES \n")
+            for oldPair in pafData:
+                ovl = oldPair.split("\t")
+                cigarIndex = [ovl.index(of) for of in ovl if of.startswith("cg")]
+                expCigar = getExpandedCigar(ovl[cigarIndex[0]].split(":")[2].strip("\n"))
+                gaps = expCigar[0].count("D") + expCigar[0].count("I")
+                subsIndex = [ovl.index(of) for of in ovl if of.startswith("NM")]
+                subsNumber = int(ovl[subsIndex[0]].split(":")[2]) - gaps
+                oldCigar.write(str(ovl[0] + "-" + ovl[5]) + "\t" + str(gaps) + "\t" + str(expCigar[0].count("M")) + "\t" + str(expCigar[0].count("D")) + "\t" + str(expCigar[0].count("I")) + "\t" + str(subsNumber) + "\n")
+        print("OLD DONE")
 os.chdir("..")
+"""
